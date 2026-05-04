ef785516534b735ae6ed73c48971f0beacd41ce8
Carlos <carlosaac232001@gmail.com>
2026-05-04 18:08:34 -0500

    style(components): remove capitalize words for topics and improve fallback links style

--- components/ProjectCard.tsx
+++ components/ProjectCard.tsx
@@ -10,7 +10,7 @@ export default function ProjectCard({
   topics,
 }: Project) {
   return (
-    <article className='flex max-h-44 flex-col justify-between rounded-xl border border-(--border) p-2.5 transition-colors duration-250 ease-out hover:border-(--border-light) max-sm:w-auto lg:p-3'>
+    <article className='flex max-h-44 flex-col justify-between rounded-xl border border-(--border) px-3.5 py-2 transition-colors duration-250 ease-out hover:border-(--border-light) max-sm:w-auto lg:px-4 lg:py-3'>
       <div>
         <h3 className='text-lg tracking-wide text-(--foreground)'>{name}</h3>
         <div className='max-h-14 overflow-y-auto text-sm text-(--gray)'>
@@ -26,13 +26,13 @@ export default function ProjectCard({
             target='_blank'
             title='A live page link to this project'
           >
-            <span className='inline-flex items-center gap-0.5 text-sm'>
+            <span className='inline-flex items-center gap-1 text-sm'>
               View website
               <SquareArrowOutUpRight size={13} />
             </span>
           </a>
         ) : (
-          <span className='text-(--gray)'>No live demo available</span>
+          <span className='text-sm text-(--gray)'>No live demo available</span>
         )}
 
         <span className='mx-2 text-(--gray)'>|</span>
@@ -45,19 +45,19 @@ export default function ProjectCard({
             target='_blank'
             title='A repository link to this project'
           >
-            <span className='inline-flex items-center gap-0.5 text-sm'>
+            <span className='inline-flex items-center gap-1 text-sm'>
               View on GitHub
               <SquareArrowOutUpRight size={13} />
             </span>
           </a>
         ) : (
-          <span className='text-(--gray)'>No live demo available</span>
+          <span className='text-sm text-(--gray)'>No live demo available</span>
         )}
       </div>
       <ul className='mt-2 flex max-h-8 flex-wrap gap-2 overflow-y-auto py-0.5 pr-2'>
         {topics.map(topic => (
           <li
-            className='rounded-md border border-(--border) px-2 py-1 text-xs text-(--foreground-secondary) capitalize'
+            className='rounded-md border border-(--border) px-2 py-1 text-xs text-(--foreground-secondary)'
             key={topic}
           >
             {topic}
