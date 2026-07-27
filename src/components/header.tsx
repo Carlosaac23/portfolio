3000d45b0f87503a132c9e36da09bd3825239a91
Carlos <carlosaac232001@gmail.com>
2026-07-26 19:08:30 -0500

    improve image loading

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -4,7 +4,10 @@ export default function Header() {
       <div class='header__div'>
         <img
           class='header__img'
-          src='/static/profile.jpg'
+          fetchpriority='high'
+          height={36}
+          width={36}
+          src='/static/profile.webp'
           alt='GitHub Profile Pic'
         />
       </div>
