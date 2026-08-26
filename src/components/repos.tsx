10a9b4c166fece7b1a1090bc306a8f6e26e1de73
Carlos <carlosaac232001@gmail.com>
2026-08-25 20:19:27 -0500

    chore: add fallback text if there is no repos

--- src/components/repos.tsx
+++ src/components/repos.tsx
@@ -5,15 +5,21 @@ import ProjectCard from './project-card.js';
 type RepoProps = { repos: Repo[] };
 
 export default function Repos({ repos }: RepoProps) {
+  const hasRepos = repos.length > 0;
+
   return (
     <section id='projects'>
       <h2 class='projects__title'>Projects</h2>
 
-      <div class='projects__container'>
-        {repos?.map(repo => (
-          <ProjectCard key={repo.name} {...repo} />
-        ))}
-      </div>
+      {hasRepos ? (
+        <div class='projects__container'>
+          {repos?.map(repo => (
+            <ProjectCard key={repo.name} {...repo} />
+          ))}
+        </div>
+      ) : (
+        <p class='projects__empty'>No projects available right now. Please check back later.</p>
+      )}
     </section>
   );
 }
